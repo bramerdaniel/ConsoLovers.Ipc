256f4dff8719d1d34231a8459c6bc5a220d5164d
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-09-22 19:39:32 +0200

    Added github action and unit test project

---
+++ src/ConsoLovers.Ipc.UnitTests/SimpleTest.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExceptionHandlingTests.cs" company="ConsoLovers">
+//    Copyright (c) ConsoLovers  2015 - 2022
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ConsoLovers.Ipc.UnitTests;
+
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+public class SimpleTest
+{
+   #region Public Methods and Operators
+
+   [TestMethod]
+   public void EnsureCustomHandlerIsInvoked()
+   {
+   }
+
+   [TestMethod]
+   public void EnsureUseExceptionHandlerWithTypeWorksCorrectly()
+   {
+      Assert.Fail("This was a test only");
+   }
+
+   #endregion
+}
